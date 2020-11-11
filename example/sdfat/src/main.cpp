b28a94e40f476f43b4d89cf3674fc756a78174ff
Timo Sandmann <mail@timosandmann.de>
2020-11-11 23:30:48 +0100

    Example for usage of SdFat library added

---
+++ example/sdfat/src/main.cpp
@@ -0,0 +1,111 @@
+/*
+ * This file is part of the FreeRTOS port to Teensy boards.
+ * Copyright (c) 2020 Timo Sandmann
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file    main.cpp
+ * @brief   FreeRTOS with SdFat library example for Teensy boards
+ * @author  Timo Sandmann
+ * @date    11.11.2020
+ */
+
+#include "arduino_freertos.h"
+#include "avr/pgmspace.h"
+
+#include "SD.h"
+
+
+static void task1(void*) {
+    while (true) {
+        ::digitalWriteFast(arduino::LED_BUILTIN, arduino::LOW);
+        ::vTaskDelay(pdMS_TO_TICKS(500));
+
+        ::digitalWriteFast(arduino::LED_BUILTIN, arduino::HIGH);
+        ::vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
+
+static void task2(void*) {
+    if (!SD.begin(BUILTIN_SDCARD)) {
+        ::Serial.println("initialization failed!");
+    } else {
+        ::Serial.println("initialization done.");
+    }
+
+    File root;
+    while (true) {
+        ::Serial.println("TICK");
+        ::vTaskDelay(pdMS_TO_TICKS(1'000));
+
+        ::Serial.println("TOCK");
+        ::vTaskDelay(pdMS_TO_TICKS(1'000));
+
+
+        root = SD.open("/");
+        if (!root.isDirectory()) {
+            ::Serial.println("open / failed!");
+            if (!SD.begin(BUILTIN_SDCARD)) {
+                ::Serial.println("initialization failed!");
+                continue;
+            }
+            ::Serial.println("initialization done.");
+            root = SD.open("/");
+            if (!root.isDirectory()) {
+                ::Serial.println("open / failed!");
+                continue;
+            }
+        }
+
+        while (true) {
+            auto entry { root.openNextFile() };
+            if (!entry) {
+                break;
+            }
+
+            ::Serial.print(entry.name());
+            if (!entry.isDirectory()) {
+                ::Serial.print("\t\t");
+                ::Serial.println(entry.size());
+            } else {
+                ::Serial.println();
+            }
+
+            entry.close();
+        }
+        root.close();
+        SD.sdfs.end();
+        ::Serial.println("\n");
+    }
+}
+
+FLASHMEM __attribute__((noinline)) void setup() {
+    ::Serial.begin(115'200);
+    ::pinMode(arduino::LED_BUILTIN, arduino::OUTPUT);
+    ::digitalWriteFast(arduino::LED_BUILTIN, arduino::HIGH);
+
+    ::delay(5'000);
+
+    ::xTaskCreate(task1, "task1", 128, nullptr, 2, nullptr);
+    ::xTaskCreate(task2, "task2", 2048, nullptr, 2, nullptr);
+
+    ::Serial.println("setup(): starting scheduler...");
+    ::Serial.flush();
+
+    ::vTaskStartScheduler();
+}
+
+void loop() {}
