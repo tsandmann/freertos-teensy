049861fbb3b0b5856733be3eff1cce413e112ed4
Timo Sandmann <mail@timosandmann.de>
2021-03-09 22:56:00 +0100

    bugfix for std::timed_mutex support

--- lib/cpp/src/bits/gthr-default.h
+++ lib/cpp/src/bits/gthr-default.h
@@ -74,7 +74,7 @@ static inline void __GTHREAD_MUTEX_INIT_FUNCTION(__gthread_mutex_t* mutex) {
 }
 
 static int __gthread_once(__gthread_once_t* once, void (*func)(void)) {
-    static __gthread_mutex_t s_m = xSemaphoreCreateMutex();
+    static __gthread_mutex_t s_m { xSemaphoreCreateMutex() };
     if (!s_m) {
         return 12; // POSIX error: ENOMEM
     }
@@ -129,13 +129,13 @@ static inline int __gthread_mutex_unlock(__gthread_mutex_t* mutex) {
 }
 
 static inline int __gthread_recursive_mutex_lock(__gthread_recursive_mutex_t* mutex) {
-    return xSemaphoreTakeRecursive(*mutex, portMAX_DELAY);
+    return (xSemaphoreTakeRecursive(*mutex, portMAX_DELAY) == pdTRUE) ? 0 : 1;
 }
 static inline int __gthread_recursive_mutex_trylock(__gthread_recursive_mutex_t* mutex) {
-    return xSemaphoreTakeRecursive(*mutex, 0);
+    return (xSemaphoreTakeRecursive(*mutex, 0) == pdTRUE) ? 0 : 1;
 }
 static inline int __gthread_recursive_mutex_unlock(__gthread_recursive_mutex_t* mutex) {
-    return xSemaphoreGiveRecursive(*mutex);
+    return (xSemaphoreGiveRecursive(*mutex) == pdTRUE) ? 0 : 1;
 }
 ////////////
 
@@ -146,39 +146,47 @@ struct __gthread_time_t {
     long sec;
     long nsec;
     int64_t milliseconds() const {
-        return static_cast<int64_t>(sec) * 1'000 + nsec / 1'000'000;
+        return static_cast<int64_t>(sec) * 1'000 + (nsec + 500'000) / 1'000'000;
     }
 };
 
 static inline __gthread_time_t operator-(const __gthread_time_t& lhs, const timeval& rhs) {
-    int32_t s { lhs.sec - rhs.tv_sec };
-    int32_t ns { lhs.nsec - rhs.tv_usec * 1'000 };
+    long s { lhs.sec - rhs.tv_sec };
+    int64_t ns { lhs.nsec - rhs.tv_usec * 1'000 };
     if (ns < 0) {
         --s;
-        ns += 1'000'000;
+        ns += 1'000'000'000;
+    } else if (ns > 1'000'000'000) {
+        ++s;
+        ns -= 1'000'000'000;
     }
 
-    return __gthread_time_t { s, ns };
+    return __gthread_time_t { s, static_cast<long>(ns) };
 }
 
 static inline int __gthread_mutex_timedlock(__gthread_mutex_t* m, const __gthread_time_t* abs_timeout) {
     timeval now {};
     gettimeofday(&now, NULL);
 
-    auto t = (*abs_timeout - now).milliseconds();
-    return xSemaphoreTake(*m, pdMS_TO_TICKS(t));
+    auto t { static_cast<int32_t>((*abs_timeout - now).milliseconds()) };
+    if (t < 0) {
+        t = 0;
+    }
+    return (xSemaphoreTake(*m, pdMS_TO_TICKS(t)) == pdTRUE) ? 0 : 1;
 }
 
-static inline int __gthread_recursive_mutex_timedlock(__gthread_recursive_mutex_t* m, const __gthread_time_t* abs_time) {
+static inline int __gthread_recursive_mutex_timedlock(__gthread_recursive_mutex_t* m, const __gthread_time_t* abs_timeout) {
     timeval now {};
     gettimeofday(&now, NULL);
 
-    auto t = (*abs_time - now).milliseconds();
-    return xSemaphoreTakeRecursive(*m, pdMS_TO_TICKS(t));
+    auto t { static_cast<int32_t>((*abs_timeout - now).milliseconds()) };
+    if (t < 0) {
+        t = 0;
+    }
+    return (xSemaphoreTakeRecursive(*m, pdMS_TO_TICKS(t)) == pdTRUE) ? 0 : 1;
 }
 
-// All functions returning int should return zero on success or the error
-//    number.  If the operation is not supported, -1 is returned.
+// All functions returning int should return zero on success or the error number.  If the operation is not supported, -1 is returned.
 
 static inline int __gthread_create(__gthread_t* thread, void (*func)(void*), void* args) {
     return thread->create_thread(func, args) ? 0 : 1;
@@ -214,13 +222,16 @@ static inline int __gthread_cond_timedwait(__gthread_cond_t* cond, __gthread_mut
     timeval now {};
     gettimeofday(&now, NULL);
 
-    auto ms { (*abs_timeout - now).milliseconds() };
+    auto ms { static_cast<int32_t>((*abs_timeout - now).milliseconds()) };
+    if (ms < 0) {
+        ms = 0;
+    }
 
     __gthread_mutex_unlock(mutex);
     auto fTimeout { 0 == ulTaskNotifyTakeIndexed(1, pdTRUE, pdMS_TO_TICKS(ms)) };
     __gthread_mutex_lock(mutex);
 
-    int result { 0 };
+    int result {};
     if (fTimeout) { // timeout - remove the thread from the waiting list
         cond->lock();
         cond->remove(this_thrd_hndl);
